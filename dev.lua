d1b733649d2ac91e695738af7fbaa84f0ce45412
Richard Weber <riche.weber@gmail.com>
2019-07-15 15:43:56 +0300

    Get the dcs_cc object out

--- dev.lua
+++ dev.lua
@@ -3,11 +3,11 @@ basePath = "C:\\Users\\Richard\\DCS_MISSIONS\\DCS_CC"
 
 function loadScript()
     assert(loadfile(basePath .. "\\dev_config.lua"))()
-    assert(loadfile(basePath .. "\\dcs_cc.lua"))()
+    return assert(loadfile(basePath .. "\\dcs_cc.lua"))()
 end
 
 -- Load it once
-loadScript()
+local dcs_cc = loadScript()
 
 
 -- run the start function
